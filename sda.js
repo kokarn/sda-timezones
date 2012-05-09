a5cc40e4afbaf82b3e3e238fb0fef02ce91fe4a4
kokarn <kokarn@gmail.com>
2012-05-09 18:44:41 +0200

    Changed so it's possible to edit timezone more than once. Also, made the calculations correct.

--- sda.js
+++ sda.js
@@ -1,37 +1,41 @@
-var defaultOffset = 5,
-    autoSet = true; // Change to not set the timezone on page load
+var timezone = parseInt('-5', 10),
+    offset = 0,
+    autoSet = true, // Change to not set the timezone on page load
+    firstDay,
+    isOriginal = true;
 
 function changeTime() {
 
     'use strict';
 
-    var day = parseInt($('.month').first().parent('tr').text().match(/\d{1,2}/), 10),
+    var day = firstDay,
         dayUpdated = false,
-        offset = defaultOffset + parseInt($('#changeTime').val(), 10);
+        newTimezone = parseInt($('#changeTime').val(), 10),
+        difference = newTimezone - timezone,
+        lastHour = 0;
 
-    $('#changeTime').attr('disabled', 'disabled');
+    //$('#changeTime').attr('disabled', 'disabled');
 
     $('.month').parent('tr').remove();
 
     $('tr').find('td:eq(2)').each(function () {
 
-        var timeData = this.innerHTML.split(' '),
-            time = timeData[0].split(':'),
-            minute = parseInt(time[1], 10),
-            hour = time[0],
-            meridian = timeData[1],
-            timeString;
+        if( isOriginal ) {
 
-        // Make sure minute is numeric
-        if (!$.isNumeric(minute)) {
-            minute = 0;
-        }
+            var timeData = this.innerHTML.split(' '),
+                time = timeData[0].split(':'),
+                minute = parseInt(time[1], 10),
+                hour = time[0],
+                meridian = timeData[1],
+                timeString;
 
-
-        if (meridian === 'AM' || meridian === 'PM') {
+            // Make sure minute is numeric
+            if (!$.isNumeric(minute)) {
+                minute = 0;
+            }
 
             hour = hour % 12 + (meridian === "AM" ? 0 : 12);
-            hour = hour + offset;
+            hour = hour + difference;
 
             if (hour > 23) {
 
@@ -50,19 +54,63 @@ function changeTime() {
                 dayUpdated = false;
             }
 
-            // Format the times to look a bit better
-            hour = hour < 10 ? "0" + hour : hour;
+        } else {
 
-            minute = minute < 10 ? "0" + minute : minute;
+            var timeData = this.innerHTML.split(' '),
+                time = timeData[1].split(':'),
+                minute = parseInt(time[1], 10),
+                hour = parseInt(time[0], 10),
+                timeString;
 
-            // Put together a nice string to output
-            timeString = day + 'th, ' + hour + ':' + minute;
+            // Make sure minute is numeric
+            if (!$.isNumeric(minute)) {
+                minute = 0;
+            }
 
-            this.innerHTML = timeString;
+            //hour = hour % 12 + (meridian === "AM" ? 0 : 12);
+            hour = hour + difference;
 
+            if(hour < 0){
+                hour += 24;
+            }
+
+            // Make sure we stay in 24-hour land
+            hour = hour % 24;
+
+            if (hour < lastHour) {
+
+                // Check if we've already counted this day or not
+                if (!dayUpdated) {
+                    day += 1;
+                    dayUpdated = true;
+                }
+
+            } else {
+
+                // It's early so this day has not been counted yet
+                dayUpdated = false;
+            }
+
+            lastHour = hour;
         }
 
+        // Format the times to look a bit better
+        hour = hour < 10 ? "0" + hour : hour;
+
+        minute = minute < 10 ? "0" + minute : minute;
+
+        // Put together a nice string to output
+        timeString = day + 'th, ' + hour + ':' + minute;
+
+        this.innerHTML = timeString;
+
     });
+
+    if( isOriginal ) {
+        isOriginal = false;
+    }
+
+    timezone = newTimezone;
 }
 $(document).ready(function () {
 
@@ -72,6 +120,8 @@ $(document).ready(function () {
         gmtHours = -d.getTimezoneOffset() / 60,
         value,
         hoursString;
+
+    firstDay = parseInt($('.month').first().parent('tr').text().match(/\d{1,2}/), 10);
 
     if (gmtHours > 0) {
         hoursString = '+' + gmtHours;
