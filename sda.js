1ac608ef9177900c586394fb226ccd28ff3537be
kokarn <kokarn@gmail.com>
2012-05-09 14:16:14 +0200

    Fixed formatting and cleaned up old code

--- sda.js
+++ sda.js
@@ -1,33 +1,42 @@
 var defaultOffset = 5;
-function changeTime(){
+
+function changeTime() {
+
+    'use strict';
+
+    var day = 4,
+        once = false,
+        offset = defaultOffset + parseInt($('#changeTime').val(), 10);
+
     $('#changeTime').attr('disabled', 'disabled');
-    var day = 4;
-    var once = false;
-    offset = defaultOffset + parseInt($('#changeTime').val());
-    $('tr').find('td:eq(2)').each(function(){
-        timeData = this.innerHTML.split(' ');
-        time = timeData[0].split(':');
-        minute = time[1];
-        hour = time[0];
-        meridian = timeData[1];
-        if(meridian == 'AM' || meridian == 'PM'){
-            if(jQuery.isNumeric(hour)){
-                hour = hour %12 + (meridian === "AM"? 0 : 12);
+
+    $('tr').find('td:eq(2)').each(function () {
+
+        var timeData = this.innerHTML.split(' '),
+            time = timeData[0].split(':'),
+            minute = time[1],
+            hour = time[0],
+            meridian = timeData[1],
+            timeString;
+
+        if (meridian === 'AM' || meridian === 'PM') {
+            if ($.isNumeric(hour)) {
+                hour = hour % 12 + (meridian === "AM" ? 0 : 12);
                 hour = hour + offset;
-                if(hour > 23){
+                if (hour > 23) {
                     hour = hour % 24;
-                    if(!once){
+                    if (!once) {
                         day++;
                         once = true;
                     }
-                }else{
+                } else {
                     once = false;
                 }
                 hour = hour < 10 ? "0" + hour : hour;
-                if(jQuery.isNumeric(minute)){
-                    timeString = day+'th '+hour+':'+minute
-                }else{
-                    timeString = day+'th '+hour+':00';
+                if ($.isNumeric(minute)) {
+                    timeString = day + 'th ' + hour + ':' + minute;
+                } else {
+                    timeString = day + 'th ' + hour + ':00';
                 }
                 this.innerHTML = timeString;
             }
@@ -35,15 +44,19 @@ function changeTime(){
 
     });
 }
-$('document').ready(function(){
-    var d = new Date();
-    var gmtHours = -d.getTimezoneOffset()/60;
+$('document').ready(function() {
+
+    'use strict';
+
+    var d = new Date(),
+        gmtHours = -d.getTimezoneOffset() / 60;
+
     $('#currentTimezone').text(gmtHours);
     $('#changeTime').change(function(){
         changeTime();
     });
 
-    if( window.location.hash !== undefined ){
+    if( window.location.hash !== undefined && window.location.hash != '' ){
         var value = window.location.hash.replace('#', '');
         $('option[value=' + value + ']').attr( 'selected', 'selected' );
         changeTime();
